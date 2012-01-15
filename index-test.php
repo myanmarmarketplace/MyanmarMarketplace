8708ec4183a903c8500fa048bdb75378dae91e94
Si Latt <myanmarmarketplace@gmail.com>
2012-01-15 14:38:39 -0500

    testing

--- index-test.php
+++ index-test.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * This is the bootstrap file for test application.
+ * The bootstrap file for test application.
  * This file should be removed when the application is deployed for production.
  */
 
